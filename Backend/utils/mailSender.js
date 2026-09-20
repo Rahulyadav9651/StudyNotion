fad0e6cba3261548ac55b4fadfa84afb84cac6aa
Rahulyadav9651 <113136972+Rahulyadav9651@users.noreply.github.com>
2026-09-20 13:10:09 +0530

    Update mailSender.js

--- Backend/utils/mailSender.js
+++ Backend/utils/mailSender.js
@@ -34,15 +34,29 @@ const nodemailer = require("nodemailer");
 
 const mailSender = async (email, title, body) => {
     try {
-        const transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
-            port: Number(process.env.MAIL_PORT) || 587,
-            secure: process.env.MAIL_SECURE === "true",
-            auth: {
-                user: process.env.MAIL_USER,
-                pass: process.env.MAIL_PASS,
-            },
-        });
+        // const transporter = nodemailer.createTransport({
+        //     host: process.env.MAIL_HOST,
+        //     port: Number(process.env.MAIL_PORT) || 587,
+        //     secure: process.env.MAIL_SECURE === "true",
+        //     auth: {
+        //         user: process.env.MAIL_USER,
+        //         pass: process.env.MAIL_PASS,
+        //     },
+        // });
+
+const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: Number(process.env.MAIL_PORT),
+    secure: process.env.MAIL_SECURE === "true",
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+    },
+});
+
+
+
+        
 
         const info = await transporter.sendMail({
             from: `"StudyNotion | CodeHelp - by Rahul" <${process.env.MAIL_USER}>`,
